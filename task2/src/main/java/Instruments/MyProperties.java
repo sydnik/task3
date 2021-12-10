0cce8e5dc32dc60334f989b5c161708355f087a0
sydnik <sydnik3@gmail.com>
2021-12-10 16:26:39 +0300

    create class for job with properties
    this class can:
    give int data
    give String
    give String-UTF-8 (for Russian symbol)

--- task2/src/main/java/Instruments/MyProperties.java
+++ task2/src/main/java/Instruments/MyProperties.java
@@ -3,6 +3,7 @@ package Instruments;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Properties;
 
 public class MyProperties {
@@ -37,12 +38,32 @@ public class MyProperties {
         }
         return myProperties;
     }
-    public String getConfiguration(String key){
+    public String getConfigurationString(String key){
         return (String) propertiesConfiguration.get(key);
     }
 
-    public String getData(String key){
+    public Integer getConfigurationInt(String key){
+        return Integer.parseInt((String) propertiesConfiguration.get(key));
+    }
+
+    public String getDataString(String key){
         return (String) propertiesData.get(key);
+    }
+
+    public Integer getDataInt(String key){
+        return Integer.parseInt((String)propertiesData.get(key));
+    }
+
+    public String getDataUTF8(String key){
+        String line = (String) propertiesData.get(key);
+        byte[] mainData = line.getBytes(StandardCharsets.ISO_8859_1);
+        return new String(mainData, StandardCharsets.UTF_8);
+    }
+
+    public String getConfigurationUTF8(String key){
+        String line = (String) propertiesData.get(key);
+        byte[] mainData = line.getBytes(StandardCharsets.ISO_8859_1);
+        return new String(mainData, StandardCharsets.UTF_8);
     }
     public void close(){
         try {
