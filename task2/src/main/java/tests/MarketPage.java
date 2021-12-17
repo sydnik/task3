fedcb338ccbe706e1966ad87a228c95aedf93f93
sydnik <sydnik3@gmail.com>
2021-12-17 11:21:48 +0300

    deleted ItemDota.java and method

--- task2/src/main/java/tests/MarketPage.java
+++ task2/src/main/java/tests/MarketPage.java
@@ -1,6 +1,5 @@
 package tests;
 
-import data.ItemDota;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -12,16 +11,16 @@ public class MarketPage extends Page{
         super();
     }
     public void isMarketPage(){
-        wait.until(ExpectedConditions.urlToBe(properties.getDataString("marketURL")));
+        wait.until(ExpectedConditions.urlToBe(properties.getDataProperty("marketURL")));
         waitVisibility(By.xpath("//div[@class='market_header_logo']"));
     }
     public void searchOnMarket(){
         By      xpathButtonSearch = By.id("market_search_advanced_show"),
                 xpathSelectGame = By.id("app_option_0_selected"),
-                xpathGame = By.xpath("//div[@id='market_advancedsearch_appselect_options']//img[@alt='" + properties.getDataString("gameSearch") +"']"),
+                xpathGame = By.xpath("//div[@id='market_advancedsearch_appselect_options']//img[@alt='" + properties.getDataProperty("gameSearch") +"']"),
                 xpathSelectHero = By.xpath("//div[@id='market_advancedsearch_filters']//select[contains(@name,'Hero')]"),
-                xpathHero = By.xpath("//div[@class='econ_tag_filter_category']//option[text()='" + properties.getDataString("hero") + "']"),
-                xpathRarity = By.xpath("//div[@id='market_advancedsearch_filters']//span[text()='" + properties.getDataString("rarity") + "']"),
+                xpathHero = By.xpath("//div[@class='econ_tag_filter_category']//option[text()='" + properties.getDataProperty("hero") + "']"),
+                xpathRarity = By.xpath("//div[@id='market_advancedsearch_filters']//span[text()='" + properties.getDataProperty("rarity") + "']"),
                 xpathTextField = By.id("advancedSearchBox"),
                 xpathButtonFind = By.xpath("//*[@class='market_advancedsearch_bottombuttons']/div");
 
@@ -31,16 +30,16 @@ public class MarketPage extends Page{
         waitClickable(xpathSelectHero).click();
         waitClickable(xpathHero).click();
         waitClickable(xpathRarity).click();
-        waitClickable(xpathTextField).sendKeys(properties.getDataString("nameThing"));
+        waitClickable(xpathTextField).sendKeys(properties.getDataProperty("nameThing"));
         waitClickable(xpathButtonFind).click();
 
     }
     public void checkFilterAndResult(){
-        By      xpathTagGame = By.xpath("//*[@class='market_search_results_header']//*[text()[contains(.,'" + properties.getDataString("gameSearch") + "')]]"),
-                xpathTagHero = By.xpath("//*[@class='market_search_results_header']//*[text()[contains(.,'" + properties.getDataString("hero") + "')]]"),
-                xpathTagRarity = By.xpath("//*[@class='market_search_results_header']//*[text()[contains(.,'" + properties.getDataString("rarity") + "')]]"),
-                xpathTagNameThing = By.xpath("//*[@class='market_search_results_header']//*[text()[contains(.,'" + properties.getDataString("nameThing") + "')]]");
-        String line = properties.getDataString("nameThing").toLowerCase();
+        By      xpathTagGame = By.xpath("//*[@class='market_search_results_header']//*[text()[contains(.,'" + properties.getDataProperty("gameSearch") + "')]]"),
+                xpathTagHero = By.xpath("//*[@class='market_search_results_header']//*[text()[contains(.,'" + properties.getDataProperty("hero") + "')]]"),
+                xpathTagRarity = By.xpath("//*[@class='market_search_results_header']//*[text()[contains(.,'" + properties.getDataProperty("rarity") + "')]]"),
+                xpathTagNameThing = By.xpath("//*[@class='market_search_results_header']//*[text()[contains(.,'" + properties.getDataProperty("nameThing") + "')]]");
+        String line = properties.getDataProperty("nameThing").toLowerCase();
 
         for (int i =0;i<5;i++){
             Assert.assertTrue(waitVisibility(By.id("result_"+i)).
@@ -53,7 +52,7 @@ public class MarketPage extends Page{
     }
     public void deleteTagForSearch(){
         WebElement removeTag;
-        String deleteTag[] = properties.getDataString("tagMarketToDelete").split("%");
+        String deleteTag[] = properties.getDataProperty("tagMarketToDelete").split("%");
         By[] xpathForDeletion = new By[deleteTag.length];
         for (int i = 0; i <deleteTag.length; i++) {
             xpathForDeletion[i] = By.xpath("//*[@class='market_search_results_header']//*[text()[contains(.,'" + deleteTag[i] + "')]]");
@@ -65,22 +64,15 @@ public class MarketPage extends Page{
             waitStalenessOf(removeTag);
         }
     }
-    public ItemDota getFirstItemAndOpen(){
-        String fullName,rarity,hero,game;
-        By      xpathFirstItem = By.id("result_0"),
-                xpathRarity = By.xpath("//*[@class='market_search_results_header']//*[text()[contains(.,'" + properties.getDataString("rarity") + "')]]"),
-                xpathHero = By.xpath("//*[@class='market_search_results_header']//*[text()[contains(.,'" + properties.getDataString("hero") + "')]]"),
-                xpathGame = By.xpath("//*[@class='market_search_results_header']//*[text()[contains(.,'" + properties.getDataString("gameSearch") + "')]]");
+    public String getFirstItemAndOpen(){
+        By      xpathFirstItem = By.id("result_0");
 
-        fullName = waitPresence(xpathFirstItem).getAttribute("data-hash-name");
-        rarity = waitPresence(xpathRarity).getText();
-        hero = waitPresence(xpathHero).getText();
-        game = waitPresence(xpathGame).getText();
+        String fullName = waitPresence(xpathFirstItem).getAttribute("data-hash-name");
         waitClickable(xpathFirstItem).click();
-        return new ItemDota(fullName,rarity,hero,game);
+        return fullName;
     }
     public void openPage(){
-        webDriver.get(properties.getDataString("marketURL"));
+        webDriver.get(properties.getDataProperty("marketURL"));
     }
 
 }
