316e5e977708b086c62c01da82ae2dadaa510a95
sydnik <sydnik3@gmail.com>
2021-12-20 10:33:42 +0300

    Transferred values to the top

--- task2/src/main/java/tests/TopSellersPage.java
+++ task2/src/main/java/tests/TopSellersPage.java
@@ -7,6 +7,24 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class TopSellersPage extends Page {
+    private final By
+            TAG_OS = By.xpath("//span[@data-loc='"+ properties.getDataProperty("tagOS") +"']"),
+            LIST_OS = By.xpath("//input[@id='os']//parent::*"),
+            HEADER_OS = By.xpath("//div[@data-collapse-name='os']"),
+            FIRST_GAME_RESULT = By.xpath("//*[@id='search_resultsRows']/a[1]"),
+            TAG_NUMBER_PLAYER = By.xpath("//span[@data-loc='"+ properties.getDataProperty("tagPlayers") +"']/span/span"),
+            LIST_NUMBER_PLAYER = By.xpath("//input[@id='category3']//parent::*"),
+            HEADER_NUMBER_PLAYER = By.xpath("//div[@data-collapse-name='category3']"),
+            TAG = By.xpath("//div[@data-loc='" + properties.getDataProperty("tags") + "']"),
+            LIST_TAG = By.xpath("//div[@id='TagFilter_Container']"),
+            TEXT_FIELD_TAG = By.id("TagSuggest"),
+            HEADER_TAG = By.xpath("//div[@data-collapse-name='tags']"),
+            LIST_GAME_RESULT = By.xpath("//*[@id='search_results']/div[1]"),
+            FIRST_GAME_NAME = By.xpath("//div[@id='search_resultsRows']//span[contains(@class,'title')]"),
+            FIRST_GAME_DATE = By.xpath("//div[@id='search_resultsRows']//div[contains(@class,'search_released')]"),
+            FIRST_GAME_PRICE = By.xpath("//div[@id='search_resultsRows']//div[contains(@class,'search_price')]"),
+            BUTTON_FIRST_GAME = By.xpath("//div[@id='search_resultsRows']//a");
+
     private int numberOfGameTags;
 
     public TopSellersPage() {
@@ -18,71 +36,50 @@ public class TopSellersPage extends Page {
     }
 
     public void selectOS(){
-        By      xpathTagOS = By.xpath("//span[@data-loc='"+ properties.getDataProperty("tagOS") +"']"),
-                xpathListOS = By.xpath("//input[@id='os']//parent::*"),
-                xpathHeaderOS = By.xpath("//div[@data-collapse-name='os']"),
-                xpathRefreshList = By.xpath("//*[@id='search_resultsRows']/a[1]");
-
-        if(!waitPresence(xpathListOS).isDisplayed()) {
-            waitClickable(xpathHeaderOS).click();
+        if(!waitPresence(LIST_OS).isDisplayed()) {
+            waitClickable(HEADER_OS).click();
         }
-        WebElement refreshList = waitVisibility(xpathRefreshList);
-        waitClickable(xpathTagOS).click();
+        WebElement refreshList = waitVisibility(FIRST_GAME_RESULT);
+        waitClickable(TAG_OS).click();
         waitStalenessOf(refreshList);
     }
 
     public void selectNumberPayers() {
-        By      xpathTagNumberOfPlayers = By.xpath("//span[@data-loc='"+ properties.getDataProperty("tagPlayers") +"']/span/span"),
-                xpathListNumberOfPlayers = By.xpath("//input[@id='category3']//parent::*"),
-                xpathHeaderNumberOfPlayers = By.xpath("//div[@data-collapse-name='category3']"),
-                xpathRefreshList = By.xpath("//*[@id='search_resultsRows']/a[1]");
-
-        if(!waitPresence(xpathListNumberOfPlayers).isDisplayed()) {
-            waitClickable(xpathHeaderNumberOfPlayers).click();
+        if(!waitPresence(LIST_NUMBER_PLAYER).isDisplayed()) {
+            waitClickable(HEADER_NUMBER_PLAYER).click();
         }
-        waitVisibility(xpathHeaderNumberOfPlayers);
-        WebElement refreshList = waitVisibility(xpathRefreshList);
-        waitClickable(xpathTagNumberOfPlayers).click();
+        waitVisibility(HEADER_NUMBER_PLAYER);
+        WebElement refreshList = waitVisibility(FIRST_GAME_RESULT);
+        waitClickable(TAG_NUMBER_PLAYER).click();
         waitStalenessOf(refreshList);
     }
 
     public void selectTags() {
         String tag = properties.getDataProperty("tags");
-        By      xpathTagsContainer = By.xpath("//div[@id='TagFilter_Container']"),
-                xpathTagsTextField = By.id("TagSuggest"),
-                xpathSelectTag = By.xpath("//div[@data-loc='" + tag + "']"),
-                xpathRefreshList = By.xpath("//*[@id='search_resultsRows']/a[1]"),
-                xpathHeaderTag = By.xpath("//div[@data-collapse-name='tags']");
-
-        if(!waitPresence(xpathTagsContainer).isDisplayed()) {
-            waitClickable(xpathHeaderTag).click();
-            waitVisibility(xpathTagsContainer);
+        if(!waitPresence(LIST_TAG).isDisplayed()) {
+            waitClickable(HEADER_TAG).click();
+            waitVisibility(LIST_TAG);
         }
-        new Actions(webDriver).moveToElement(waitClickable(xpathTagsTextField)).click().sendKeys(tag).build().perform();
-        WebElement selectTag  = waitVisibility(xpathSelectTag);
+        new Actions(webDriver).moveToElement(waitClickable(TEXT_FIELD_TAG)).click().sendKeys(tag).build().perform();
+        WebElement selectTag  = waitVisibility(TAG);
         numberOfGameTags = Integer.parseInt(selectTag.getText().replaceAll("[^0-9]",""));
-        WebElement elementGame = waitVisibility(xpathRefreshList);
-        waitClickable(xpathSelectTag).click();
+        WebElement elementGame = waitVisibility(FIRST_GAME_RESULT);
+        waitClickable(TAG).click();
         waitStalenessOf(elementGame);
     }
 
     public void checkTags() {
-        By xpathResultGame = By.xpath("//*[@id='search_results']/div[1]");
-        wait.until(ExpectedConditions.textToBePresentInElementLocated(xpathResultGame, String.valueOf(numberOfGameTags)));
+        wait.until(ExpectedConditions.textToBePresentInElementLocated(LIST_GAME_RESULT, String.valueOf(numberOfGameTags)));
     }
     public Game getFirstGame(){
-        By      xpathName = By.xpath("//div[@id='search_resultsRows']//span[contains(@class,'title')]"),
-                xpathDateReleased = By.xpath("//div[@id='search_resultsRows']//div[contains(@class,'search_released')]"),
-                xpathPrice = By.xpath("//div[@id='search_resultsRows']//div[contains(@class,'search_price')]");
-
-        String name = waitPresence(xpathName).getText();
-        String date = waitPresence(xpathDateReleased).getText();
-        String[] priceList = waitPresence(xpathPrice).getText().split("\n");
+        String name = waitPresence(FIRST_GAME_NAME).getText();
+        String date = waitPresence(FIRST_GAME_DATE).getText();
+        String[] priceList = waitPresence(FIRST_GAME_PRICE).getText().split("\n");
         String price = priceList[priceList.length-1].replace("$","");
         return new Game(name,date,price);
     }
     public void openGamePage(){
-        waitClickable(By.xpath("//div[@id='search_resultsRows']//a")).click();
+        waitClickable(BUTTON_FIRST_GAME).click();
     }
 
 }
